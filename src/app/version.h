879acdf3d5b98f48d4227455decf58afbd7d0d93
yunbing_xu <yunbing_xu@qq.com>
2025-01-13 16:23:42 +0800

    feat: 忽略 version.h 文件, 因为它是由 cmake 自动生成的

--- src/app/version.h
+++
@@ -1,9 +0,0 @@
-#ifndef _VERSION_H___
-#define _VERSION_H___
-
-#define APP_VERSION      "1.0.0"
-#define APP_FULL_VERSION "1.0.0.unknown"
-#define GIT_COMMIT_COUNT "000000"
-#define GIT_COMMIT_HASH  "unknown"
-
-#endif /* _VERSION_H___ */

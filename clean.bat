0b1a0ad61dbb0d4bfe1fa700ea2b50f6dcdeeee8
yunbing_xu <yunbing_xu@qq.com>
2025-06-30 16:29:58 +0800

    feat: 优化清除脚本

--- clean.bat
+++ clean.bat
@@ -1,13 +1,15 @@
 @echo off
-echo Cleaning temporary files, please wait...
+echo Cleaning up temporary files, please wait...
 
 del /f /s /q .\*.d
 del /f /s /q .\*.obj
 del /f /s /q .\*.o
-del /f /s /q .\*.pdb
 del /f /s /q .\*.tlog
 del /f /s /q .\*.VC.db
+del /f /s /q .\*.vcxproj
+del /f /s /q .\*.vcxproj.filters
+del /f /s /q .\*.sln
 del /f /s /q .\*CMakeCache.txt
 
-echo Cleanup completed!
+echo Clean up completed!
 @REM echo. & pause

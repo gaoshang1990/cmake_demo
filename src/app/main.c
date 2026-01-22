f85e427a9530411dc66b5199f74cb1c92074e1de
yunbing_xu <yunbing_xu@qq.com>
2026-01-22 17:46:09 +0800

    feat: 使用 CMakePresets.json 配置交叉编译

--- src/app/main.c
+++ src/app/main.c
@@ -1,4 +1,6 @@
 
+#include <MQTTClient.h>
+
 #include "utils_log.h"
 #include "version.h"
 
@@ -7,6 +9,17 @@ int main()
 {
     slog_init(M_DEBUG, "./", "app.log");
     slog_app_info("cmake_demo", APP_FULL_VERSION);
+
+    MQTTClient _client;
+
+    int rc = MQTTClient_create(&_client, "localhost:1883", "test_id", MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    if (rc != MQTTCLIENT_SUCCESS) {
+        slog_error("failed to create client, return code %d", rc);
+    }
+    else {
+        slog_info("MQTT client created successfully");
+    }
+
 
     return 0;
 }
